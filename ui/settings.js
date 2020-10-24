d9a70caa281c5a23f269c143752bd541c26124a1
iMplode nZ <rg@youxplode.com>
2020-10-24 15:14:33 -0700

    Added settings file.

---
+++ ui/settings.js
@@ -0,0 +1,13 @@
+const m = require('mithril');
+const HTML = require('hyperscript-adapter');
+const HTMLModule = require('hyperscript-adapter');
+const $ = HTML({
+    h: m,
+    textConvert: (a) => `${a}`,
+    combineId: true,
+    combineClasses: true,
+});
+const mod = HTMLModule($, {
+    partialApply: true,
+});
+module.exports = { mod, $ };
